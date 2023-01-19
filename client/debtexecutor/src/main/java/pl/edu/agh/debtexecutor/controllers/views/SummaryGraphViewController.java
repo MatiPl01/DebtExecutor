6f3ae7bc0436aca9fee2b8e13606c3a7e64c9978
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-01-19 20:30:30 +0100

    Add graph library

--- client/debtexecutor/src/main/java/pl/edu/agh/debtexecutor/controllers/views/SummaryGraphViewController.java
+++ client/debtexecutor/src/main/java/pl/edu/agh/debtexecutor/controllers/views/SummaryGraphViewController.java
@@ -1,7 +1,11 @@
 package pl.edu.agh.debtexecutor.controllers.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SummaryGraphViewController extends SummaryViewAbstractController {
+    @FXML
+    private AnchorPane graphWrapper;
 }
